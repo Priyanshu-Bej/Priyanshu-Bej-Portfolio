6d105862cbbe5599c1507d34db563700c479c4de
Priyanshu Bej <100060162+Priyanshu-Bej@users.noreply.github.com>
2024-10-26 05:30:05 +0530

    Updated Resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -13,7 +13,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 // Replace these with your actual image and resume link
 const resumeDetails = {
   image: "https://github.com/Priyanshu-Bej/Priyanshu-Pritam-Bej-Resume/blob/main/Priyanshu_Pritam_Bej_Resume_Thumbnail.png?raw=true", // Replace with your resume image path
-  resume_link: "https://drive.google.com/file/d/1O6ZNfHPqNeRDpWb0_3Ab_H50AUsTfExL/view?usp=sharing", // Replace with your resume link
+  resume_link: "https://drive.google.com/file/d/1kDkVnTFTp0X3BzCZM9t-GH2AM1sPaIxl/view?usp=sharing", //New Updaeted Resume
   title: "Inspect Resume",
   description:
     "Click to view my resume.",
